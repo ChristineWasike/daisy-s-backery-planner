b9444eb5c74cfe40cb0eb23a23c4dc2565c36b3a
ChristineWasike <christinewasike22@gmail.com>
2020-04-23 09:35:39 +0300

    Added the attributes of the Order object

--- models/Order.py
+++ models/Order.py
@@ -1,2 +1,7 @@
 class Order:
-    def __init__(self, title, ):
+    # These are the attributes that each order must have to be initialised
+    def __init__(self, title, time, assigned_to, priority):
+        self.title = title
+        self.time = time
+        self.assigned_to = assigned_to
+        self.priority = priority
