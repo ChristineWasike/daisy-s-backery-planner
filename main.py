de55805f2b9685f7de4f62ccc69b7699970533db
ChristineWasike <christinewasike22@gmail.com>
2020-04-23 09:46:50 +0300

    Added some TODOs  in the functions(possibly temporary) file

---
+++ main.py
@@ -0,0 +1,7 @@
+"""
+Here, we need to run the entire scheduler from when Daisy inputs her orders,
+when she views the orders she has for the week, updates the order based on
+what she's been able to work on to clearing orders upon successful delivery and payment.
+"""
+
+# TODO run the entire program from here
